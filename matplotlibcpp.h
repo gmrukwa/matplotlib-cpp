db2ca5a4eb7c449834e7a5ce03ba6e4819674102
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-04-20 07:37:20 +0200

    Extract stem

--- matplotlibcpp.h
+++ matplotlibcpp.h
@@ -18,37 +18,10 @@
 #include "annotate.h"
 #include "_numpy.h"
 #include "plot.h"
+#include "stem.h"
 
 
 namespace matplotlibcpp {
-template<typename Numeric>
-bool stem(const std::vector<Numeric> &x, const std::vector<Numeric> &y, const std::map<std::string, std::string>& keywords)
-{
-    assert(x.size() == y.size());
-
-    // using numpy arrays
-    PyObject* xarray = get_array(x);
-    PyObject* yarray = get_array(y);
-
-    // construct positional args
-    PyObject* args = PyTuple_New(2);
-    PyTuple_SetItem(args, 0, xarray);
-    PyTuple_SetItem(args, 1, yarray);
-
-    // construct keyword args
-    PyObject* kwargs = helpers::to_dict(keywords);
-
-    PyObject* res = PyObject_Call(
-            detail::_interpreter::get().s_python_function_stem, args, kwargs);
-
-    Py_DECREF(args);
-    Py_DECREF(kwargs);
-    if (res != nullptr)
-        Py_DECREF(res);
-
-    return res != nullptr;
-}
-
 template< typename Numeric >
 bool fill_between(const std::vector<Numeric>& x, const std::vector<Numeric>& y1, const std::vector<Numeric>& y2, const std::map<std::string, std::string>& keywords)
 {
