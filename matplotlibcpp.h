8d1f4027a688344a882ee9a03d04b09b2515dfbd
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-04-20 07:38:48 +0200

    Move stem overloads

--- matplotlibcpp.h
+++ matplotlibcpp.h
@@ -25,31 +25,6 @@
 
 namespace matplotlibcpp {
 template<typename NumericX, typename NumericY>
-bool stem(const std::vector<NumericX>& x, const std::vector<NumericY>& y, const std::string& s = "")
-{
-    assert(x.size() == y.size());
-
-    PyObject* xarray = get_array(x);
-    PyObject* yarray = get_array(y);
-
-    PyObject* pystring = PyString_FromString(s.c_str());
-
-    PyObject* plot_args = PyTuple_New(3);
-    PyTuple_SetItem(plot_args, 0, xarray);
-    PyTuple_SetItem(plot_args, 1, yarray);
-    PyTuple_SetItem(plot_args, 2, pystring);
-
-    PyObject* res = PyObject_CallObject(
-            detail::_interpreter::get().s_python_function_stem, plot_args);
-
-    Py_DECREF(plot_args);
-    if (res != nullptr)
-        Py_DECREF(res);
-
-    return res != nullptr;
-}
-
-template<typename NumericX, typename NumericY>
 bool semilogx(const std::vector<NumericX>& x, const std::vector<NumericY>& y, const std::string& s = "")
 {
     assert(x.size() == y.size());
@@ -223,14 +198,6 @@ bool named_loglog(const std::string& name, const std::vector<Numeric>& x, const 
     if (res != nullptr) Py_DECREF(res);
 
     return res != nullptr;
-}
-
-template<typename Numeric>
-bool stem(const std::vector<Numeric>& y, const std::string& format = "")
-{
-    std::vector<Numeric> x(y.size());
-    for (size_t i = 0; i < x.size(); ++i) x.at(i) = static_cast<Numeric>(i);
-    return stem(x, y, format);
 }
 
 inline void figure()
--- stem.h
+++ stem.h
@@ -32,4 +32,37 @@ bool stem(const std::vector<Numeric> &x, const std::vector<Numeric> &y, const st
 
     return res != nullptr;
 }
+
+template<typename NumericX, typename NumericY>
+bool stem(const std::vector<NumericX>& x, const std::vector<NumericY>& y, const std::string& s = "")
+{
+    assert(x.size() == y.size());
+
+    PyObject* xarray = get_array(x);
+    PyObject* yarray = get_array(y);
+
+    PyObject* pystring = PyString_FromString(s.c_str());
+
+    PyObject* plot_args = PyTuple_New(3);
+    PyTuple_SetItem(plot_args, 0, xarray);
+    PyTuple_SetItem(plot_args, 1, yarray);
+    PyTuple_SetItem(plot_args, 2, pystring);
+
+    PyObject* res = PyObject_CallObject(
+            detail::_interpreter::get().s_python_function_stem, plot_args);
+
+    Py_DECREF(plot_args);
+    if (res != nullptr)
+        Py_DECREF(res);
+
+    return res != nullptr;
+}
+
+template<typename Numeric>
+bool stem(const std::vector<Numeric>& y, const std::string& format = "")
+{
+    std::vector<Numeric> x(y.size());
+    for (size_t i = 0; i < x.size(); ++i) x.at(i) = static_cast<Numeric>(i);
+    return stem(x, y, format);
+}
 } // end namespace matplotlibcpp
