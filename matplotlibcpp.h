97e554ef380486514dbad22c7b4ac80b7ac40bc6
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-04-20 07:37:55 +0200

    Extract hist

--- matplotlibcpp.h
+++ matplotlibcpp.h
@@ -20,36 +20,10 @@
 #include "plot.h"
 #include "stem.h"
 #include "fill_between.h"
+#include "hist.h"
 
 
 namespace matplotlibcpp {
-template< typename Numeric>
-bool hist(const std::vector<Numeric>& y, long bins=10,std::string color="b", double alpha=1.0)
-{
-
-    PyObject* yarray = get_array(y);
-
-    PyObject* kwargs = PyDict_New();
-    PyDict_SetItemString(kwargs, "bins", PyLong_FromLong(bins));
-    PyDict_SetItemString(kwargs, "color", PyString_FromString(color.c_str()));
-    PyDict_SetItemString(kwargs, "alpha", PyFloat_FromDouble(alpha));
-
-
-    PyObject* plot_args = PyTuple_New(1);
-
-    PyTuple_SetItem(plot_args, 0, yarray);
-
-
-    PyObject* res = PyObject_Call(detail::_interpreter::get().s_python_function_hist, plot_args, kwargs);
-
-
-    Py_DECREF(plot_args);
-    Py_DECREF(kwargs);
-    if(res != nullptr) Py_DECREF(res);
-
-    return res != nullptr;
-}
-
 template< typename Numeric>
 bool named_hist(std::string label,const std::vector<Numeric>& y, long bins=10, std::string color="b", double alpha=1.0)
 {
