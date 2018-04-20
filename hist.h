1f6a6480f2ffd4e647e7f6221a5ee32d31912471
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-04-20 07:38:16 +0200

    Move named_hist to hist.h

--- hist.h
+++ hist.h
@@ -31,4 +31,28 @@ bool hist(const std::vector<Numeric>& y, long bins=10,std::string color="b", dou
 
     return res != nullptr;
 }
+
+template< typename Numeric>
+bool named_hist(std::string label,const std::vector<Numeric>& y, long bins=10, std::string color="b", double alpha=1.0)
+{
+    PyObject* yarray = get_array(y);
+
+    PyObject* kwargs = PyDict_New();
+    PyDict_SetItemString(kwargs, "label", PyString_FromString(label.c_str()));
+    PyDict_SetItemString(kwargs, "bins", PyLong_FromLong(bins));
+    PyDict_SetItemString(kwargs, "color", PyString_FromString(color.c_str()));
+    PyDict_SetItemString(kwargs, "alpha", PyFloat_FromDouble(alpha));
+
+
+    PyObject* plot_args = PyTuple_New(1);
+    PyTuple_SetItem(plot_args, 0, yarray);
+
+    PyObject* res = PyObject_Call(detail::_interpreter::get().s_python_function_hist, plot_args, kwargs);
+
+    Py_DECREF(plot_args);
+    Py_DECREF(kwargs);
+    if(res != nullptr) Py_DECREF(res);
+
+    return res != nullptr;
+}
 } // end namespace matplotlibcpp
